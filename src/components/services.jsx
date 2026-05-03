c65f1420a802c4b9e90df99e26715622de72a88c
Hunter <ed2513512@gmail.com>
2026-05-03 15:01:39 +0000

    Initial commit

---
+++ src/components/services.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export class Services extends Component {
+  render() {
+    return (
+      <div id="services" className="text-center">
+        <div className="container">
+          <div className="section-title">
+            <h2>Our Services</h2>
+            <p>
+              We'll help you view, find, and travel to parking spaces with
+              Real-Time Availability, Integrated Smart Search, and more.
+            </p>
+          </div>
+
+          <div className="row">
+            {this.props.data ? (
+              this.props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p>Loading services...</p>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Services;
